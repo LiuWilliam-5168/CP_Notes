5882139a6987aa0284afce22fc150c185d01f4be
LiuWilliam <90632091+LiuWilliam-5168@users.noreply.github.com>
2022-04-03 22:53:00 +0800

    Update Dijkstra_heap.cpp

--- Dijkstra_heap.cpp
+++ Dijkstra_heap.cpp
@@ -1,23 +1,24 @@
 #include <bits/stdc++.h>
 
 #define int long long
-#define pp pair <int, int>
+#define pii pair <int, int>
 #define ff first
 #define ss second
 const int INF = 1e9;
 using namespace std;
 
-vector <int> Dijkstra(const vector <vector <pp>> &g, int start) {   // pair <weight of edge, neighbor> 
-    priority_queue <pp, vector <pp>, greater <pp>> pq;
+vector <int> Dijkstra(const vector <vector <pii>> &g, int start) {   // pair <weight of edge, neighbor> 
+    priority_queue <pii, vector <pii>, greater <pii>> pq;
     vector <int> dis(g.size(), INF);
     pq.push({0, start});
     
     while (!pq.empty()) {
-        pp u = pq.top();  pq.pop();
-        if (dis[u.ss] != INF) continue;
-        dis[u.ss] = u.ff;
-        for (pp e : g[u.ss]) {
-            pq.push({e.ff + dis[u.ss], e.ss});
+        auto tmp = pq.top();  pq.pop();
+        int cnt = tmp.ff, u = tmp.ss;
+        if (dis[u] != INF) continue;
+        dis[u] = cnt;
+        for (auto e : g[u]) {
+            pq.push({e.ff + dis[u], e.ss});
         }
     }
 
