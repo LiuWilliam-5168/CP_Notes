4e77d6dc54e987c4918d5b865109725b6f371d81
LiuWilliam <90632091+LiuWilliam-5168@users.noreply.github.com>
2022-08-04 21:47:34 +0800

    Update 離散化.cpp

--- templates/搜尋/離散化.cpp
+++ templates/搜尋/離散化.cpp
@@ -1,7 +1,4 @@
-int n;
-vector <int> ar(maxn);
-
-void dis_bs() {
+void dis_bs(vector <int> &ar) {
     vector <int> tmp(ar, ar + n);
     sort(tmp.begin(), tmp.end());
     tmp.resize(unique(tmp.begin(), tmp.end()) - tmp.begin());
