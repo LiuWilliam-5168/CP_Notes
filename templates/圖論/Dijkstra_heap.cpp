3eb6e997ff87570e4baa8bb486a838ad38ca9ea0
LiuWilliam <90632091+LiuWilliam-5168@users.noreply.github.com>
2022-08-11 22:39:36 +0800

    Update Dijkstra_heap.cpp

--- templates/圖論/Dijkstra_heap.cpp
+++ templates/圖論/Dijkstra_heap.cpp
@@ -1,17 +1,18 @@
-vector <int> dijkstra(const vector <vector <pii>> &e, int start) {   // {neighbor, weight} 
-    priority_queue <pii, vector <pii>, greater <pii>> pq;
-    vector <int> dis(n, INF);
-    
-    pq.push({0, start});
-    while (!pq.empty()) {
-        auto [d, u] = pq.top();  pq.pop();
-        
-        if (dis[u] != INF) continue;
-        dis[u] = cnt;
-        for (auto [v, w] : e[u]) {
-            pq.push({dis[u] + w, v});
-        }
-    }
-
-    return dis;
+void dijkstra() {
+	priority_queue <pii, vector <pii>, greater <>> pq;
+    vector <int> dis(maxn, INF);
+ 
+	pq.push({0, 1});
+	while (!pq.empty()) {
+		auto [d, u] = pq.top();  pq.pop();
+		
+		if (dis[u] <= d) continue;
+		dis[u] = d;
+ 
+		for (auto [v, w] : e[u]) {
+			if (d + w < dis[v]) {
+				pq.push({d + w, v});
+			}
+		}
+	}
 }
