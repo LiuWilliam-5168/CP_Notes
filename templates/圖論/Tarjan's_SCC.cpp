448c0e59f89c9385cce627d44d725fe3bd9e474e
LiuWilliam <90632091+LiuWilliam-5168@users.noreply.github.com>
2022-08-11 22:52:29 +0800

    Create Tarjan's_SCC.cpp

---
+++ templates/圖論/Tarjan's_SCC.cpp
@@ -0,0 +1,30 @@
+int dfn[maxn], low[maxn], scc[maxn], sccid = 0, ord = 0;
+vector <int> e[maxn];
+stack <int> stk;
+bitset <maxn> instk;
+ 
+void dfs(int u) {
+	dfn[u] = low[u] = ++ord;
+	stk.push(u);
+	instk[u] = true;
+	
+	for (int v : e[u]) {
+		if (!dfn[v]) {
+			dfs(v);
+			low[u] = min(low[u], low[v]);
+		} else if (instk[v]) {
+			low[u] = min(low[u], dfn[v]);
+		}
+	}
+	
+	if (dfn[u] == low[u]) {
+		sccid++;
+		
+		int x;
+		do {
+			x = stk.top();  stk.pop();
+			scc[x] = sccid;
+			instk[x] = false;
+		} while (x != u);
+	}
+}
