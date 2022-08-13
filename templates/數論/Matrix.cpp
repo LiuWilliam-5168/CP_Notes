7d7e441284ef95eff410628a1123bda44489bf2d
LiuWilliam <90632091+LiuWilliam-5168@users.noreply.github.com>
2022-08-13 17:07:21 +0800

    Update Matrix.cpp

--- templates/數論/Matrix.cpp
+++ templates/數論/Matrix.cpp
@@ -1,32 +1,30 @@
-// 單位矩陣
-Matrix identity() {
-    Matrix rtn;
-    for (int i = 0; i < n; i++) {
-        rtn[i][i] = 1;
-    }
-    return rtn;
-}
-
 // 矩陣乘法
-Matrix operator * (const Matrix &a, const Matrix &b) {
-    Matrix c;
-    for (int i = 0; i < a.size(); i++) {
-        for (int j = 0; j < b[0].size(); j++) {
-            for (int k = 0; k < b.size(); k++) {
-                c[i][j] += a[i][k] * b[k][j] % MOD;
+struct Matrix {
+    int a[maxn][maxn] = {};
+    Matrix operator * (const Matrix &b) {
+        Matrix c;
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                for (int k = 0; k < n; k++) {
+                    c.a[i][j] += a[i][k] * b.a[k][j];
+                }
+                c.a[i][j] %= MOD;
             }
-            c[i][j] %= MOD;
         }
+        return c;
     }
-    return c;
-}
+};
 
 // 矩陣快速冪
 Matrix fstpow_matrix(Matrix a, int p) {
-    if (p == 0) return I;
-    if (p == 1) return a;
-
-    Matrix rtn = fstpow_matrix(a, p >> 1);
-    if (p & 1) return rtn * rtn * a;
-    else return rtn * rtn;
+    Matrix rtn;
+    for (int i = 0; i < n; i++) rtn.a[i][i] = 1;
+    while (p > 0) {
+        if (p & 1) {
+            rtn = rtn * a;
+        }
+        a = a * a;
+        p >>= 1;
+    }
+    return rtn;
 }
