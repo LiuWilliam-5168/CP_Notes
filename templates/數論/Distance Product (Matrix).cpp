05766b09b61e97610d391fbd94e255439e410b00
LiuWilliam <90632091+LiuWilliam-5168@users.noreply.github.com>
2022-08-13 17:05:29 +0800

    Update Distance Product (Matrix).cpp

--- templates/數論/Distance Product (Matrix).cpp
+++ templates/數論/Distance Product (Matrix).cpp
@@ -2,9 +2,9 @@ Matrix operator * (const Matrix &a, const Matrix &b) {
     Matrix c;
     for (int i = 0; i < n; i++) {
         for (int j = 0; j < n; j++) {
-            c[i][j] = INF;
+            c.a[i][j] = INF;
             for (int k = 0; k < n; k++) {
-                c[i][j] = min(c[i][j], a[i][k] + b[k][j]);
+                c.a[i][j] = min(c.a[i][j], a.a[i][k] + b.a[k][j]);
             }
         }
     }
