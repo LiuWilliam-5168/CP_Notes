4adcbb68ff24d6b0cda54e59eeb0b2306233c248
LiuWilliam <90632091+LiuWilliam-5168@users.noreply.github.com>
2022-09-29 22:39:08 +0800

    Create Z_Algorithm.cpp

---
+++ templates/字串/Z_Algorithm.cpp
@@ -0,0 +1,23 @@
+int n;
+string s;   // 1-based (0-based 也可以)
+ 
+int z[maxn];
+ 
+void Z_algo() {
+    int l = 1, r = 1;
+    for (int i = 2; i <= n; i++) {
+        if (s[i] == s[i - l + 1] && i + z[i - l + 1] <= r) {
+            z[i] = z[i - l + 1];
+        } else {
+            l = i, r = max(i, r);
+            while (r <= n && s[r] == s[r - i + 1]) ++r;
+            z[i] = r - l;
+            --r;
+        }
+    }
+    
+    for (int i = 1; i <= n; i++) {
+        cout << z[i] << ' ';
+    }
+    cout << "\n";
+}
