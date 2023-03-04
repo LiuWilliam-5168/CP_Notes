4315f06dc4da455b3ef428f01d6dca9c2d53ec59
LiuWilliam <90632091+LiuWilliam-5168@users.noreply.github.com>
2023-03-04 22:56:11 +0800

    Update KMP_Algorithm.cpp

--- templates/字串/KMP_Algorithm.cpp
+++ templates/字串/KMP_Algorithm.cpp
@@ -3,7 +3,7 @@ string s;   // 1-based
  
 int kmp[maxn];
  
-void do_KMP() {
+void KMP() {
     for (int i = 2, now = 0; i <= n; i++) {
         while (now > 0 && s[i] != s[now + 1]) now = kmp[now];
         if (s[i] == s[now + 1]) {
